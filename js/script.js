3fa24826857f1abd463bd5aa17d3d9126907e314
Ntwom <mgmgnyanminmg@gmail.com>
2019-08-23 18:37:42 +0630

    Complete Main Page!

--- js/script.js
+++ js/script.js
@@ -21,154 +21,154 @@ function get(obj){
 }
 
 //Header Form Tabs Script
-//Form jobs functions start
-function form_switchJobs(){
-	get('form_jobs_tab').style.background = "#3a9bfa";
-	get('form_jobs_tab').style.color = "#fff";
-	get('form_place_tab').style.color = "#000";
-	get('form_flatmate_tab').style.color = "#000";
-	get('form_place_tab').style.background = "#fff";
-	get('form_flatmate_tab').style.background = "#fff";
-	get('form_jobs').style.display = "block";
-	get('form_place').style.display = "none";
-	get('form_flatmate').style.display = "none";
+//Form functions start
+function form_switchOne(){
+	get('tab_one').style.background = "#3a9bfa";
+	get('tab_one').style.color = "#fff";
+	get('tab_two').style.color = "#000";
+	get('tab_three').style.color = "#000";
+	get('tab_two').style.background = "#fff";
+	get('tab_three').style.background = "#fff";
+	get('form_one').style.display = "block";
+	get('form_two').style.display = "none";
+	get('form_three').style.display = "none";
 }
 
-function form_switchPlace(){
-	get('form_place_tab').style.background = "#3a9bfa";
-	get('form_place_tab').style.color = "#fff";
-	get('form_jobs_tab').style.color = "#000";
-	get('form_flatmate_tab').style.color = "#000";
-	get('form_jobs_tab').style.background = "#fff";
-	get('form_flatmate_tab').style.background = "#fff";
-	get('form_place').style.display = "block";
-	get('form_jobs').style.display = "none";
-	get('form_flatmate').style.display = "none";
+function form_switchTwo(){
+	get('tab_two').style.background = "#3a9bfa";
+	get('tab_two').style.color = "#fff";
+	get('tab_one').style.color = "#000";
+	get('tab_three').style.color = "#000";
+	get('tab_one').style.background = "#fff";
+	get('tab_three').style.background = "#fff";
+	get('form_two').style.display = "block";
+	get('form_one').style.display = "none";
+	get('form_three').style.display = "none";
 }
 
-function form_switchFlatmate(){
-	get('form_flatmate_tab').style.background = "#3a9bfa";
-	get('form_flatmate_tab').style.color = "#fff";
-	get('form_jobs_tab').style.color = "#000";
-	get('form_place_tab').style.color = "#000";
-	get('form_jobs_tab').style.background = "#fff";
-	get('form_place_tab').style.background = "#fff";
-	get('form_place').style.display = "none";
-	get('form_jobs').style.display = "none";
-	get('form_flatmate').style.display = "block";
+function form_switchThree(){
+	get('tab_three').style.background = "#3a9bfa";
+	get('tab_three').style.color = "#fff";
+	get('tab_one').style.color = "#000";
+	get('tab_two').style.color = "#000";
+	get('tab_one').style.background = "#fff";
+	get('tab_two').style.background = "#fff";
+	get('form_two').style.display = "none";
+	get('form_one').style.display = "none";
+	get('form_three').style.display = "block";
 }
 
 
 //Body Category Tabs Script!
-//How works functions start!
-function work_switchPlace(){
-	get('how_works_place').style.background = "#3a9bfa";
-	get('how_works_place').style.color = "#fff";
-	get('how_works_jobs').style.color = "#000";
-	get('how_works_flatmate').style.color = "#000";
-	get('how_works_jobs').style.background = "#fff";
-	get('how_works_flatmate').style.background = "#fff";
-	get('how_works_panel_place').style.display = "block";
-	get('how_works_panel_jobs').style.display = "none";
-	get('how_works_panel_flatmate').style.display = "none";
+//How One functions start!
+function work_switchOne(){
+	get('how_works_one').style.background = "#3a9bfa";
+	get('how_works_one').style.color = "#fff";
+	get('how_works_two').style.color = "#000";
+	get('how_works_three').style.color = "#000";
+	get('how_works_two').style.background = "#fff";
+	get('how_works_three').style.background = "#fff";
+	get('how_works_panel_one').style.display = "block";
+	get('how_works_panel_two').style.display = "none";
+	get('how_works_panel_three').style.display = "none";
 }
 
-function work_switchJobs(){
-	get('how_works_jobs').style.background = "#3a9bfa";
-	get('how_works_jobs').style.color = "#fff";
-	get('how_works_place').style.color = "#000";
-	get('how_works_flatmate').style.color = "#000";
-	get('how_works_place').style.background = "#fff";
-	get('how_works_flatmate').style.background = "#fff";
-	get('how_works_panel_place').style.display = "none";
-	get('how_works_panel_jobs').style.display = "block";
-	get('how_works_panel_flatmate').style.display = "none";
+function work_switchTwo(){
+	get('how_works_two').style.background = "#3a9bfa";
+	get('how_works_two').style.color = "#fff";
+	get('how_works_one').style.color = "#000";
+	get('how_works_three').style.color = "#000";
+	get('how_works_one').style.background = "#fff";
+	get('how_works_three').style.background = "#fff";
+	get('how_works_panel_one').style.display = "none";
+	get('how_works_panel_two').style.display = "block";
+	get('how_works_panel_three').style.display = "none";
 }
 
-function work_switchFlatmate(){
-	get('how_works_flatmate').style.background = "#3a9bfa";
-	get('how_works_flatmate').style.color = "#fff";
-	get('how_works_jobs').style.color = "#000";
-	get('how_works_place').style.color = "#000";
-	get('how_works_jobs').style.background = "#fff";
-	get('how_works_place').style.background = "#fff";
-	get('how_works_panel_place').style.display = "none";
-	get('how_works_panel_jobs').style.display = "none";
-	get('how_works_panel_flatmate').style.display = "block";
+function work_switchThree(){
+	get('how_works_three').style.background = "#3a9bfa";
+	get('how_works_three').style.color = "#fff";
+	get('how_works_two').style.color = "#000";
+	get('how_works_one').style.color = "#000";
+	get('how_works_two').style.background = "#fff";
+	get('how_works_one').style.background = "#fff";
+	get('how_works_panel_one').style.display = "none";
+	get('how_works_panel_two').style.display = "none";
+	get('how_works_panel_three').style.display = "block";
 }
 
-//How Jobs functions start!
-function jobs_switchPlace(){
-	get('how_jobs_place').style.background = "#3a9bfa";
-	get('how_jobs_place').style.color = "#fff";
-	get('how_jobs_jobs').style.color = "#000";
-	get('how_jobs_flatmate').style.color = "#000";
-	get('how_jobs_jobs').style.background = "#fff";
-	get('how_jobs_flatmate').style.background = "#fff";
-	get('how_jobs_panel_place').style.display = "block";
-	get('how_jobs_panel_jobs').style.display = "none";
-	get('how_jobs_panel_flatmate').style.display = "none";
+//How Two functions start!
+function jobs_switchOne(){
+	get('how_jobs_one').style.background = "#3a9bfa";
+	get('how_jobs_one').style.color = "#fff";
+	get('how_jobs_two').style.color = "#000";
+	get('how_jobs_three').style.color = "#000";
+	get('how_jobs_two').style.background = "#fff";
+	get('how_jobs_three').style.background = "#fff";
+	get('how_jobs_panel_one').style.display = "block";
+	get('how_jobs_panel_two').style.display = "none";
+	get('how_jobs_panel_three').style.display = "none";
 }
 
-function jobs_switchJobs(){
-	get('how_jobs_jobs').style.background = "#3a9bfa";
-	get('how_jobs_jobs').style.color = "#fff";
-	get('how_jobs_place').style.color = "#000";
-	get('how_jobs_flatmate').style.color = "#000";
-	get('how_jobs_place').style.background = "#fff";
-	get('how_jobs_flatmate').style.background = "#fff";
-	get('how_jobs_panel_place').style.display = "none";
-	get('how_jobs_panel_jobs').style.display = "block";
-	get('how_jobs_panel_flatmate').style.display = "none";
+function jobs_switchTwo(){
+	get('how_jobs_two').style.background = "#3a9bfa";
+	get('how_jobs_two').style.color = "#fff";
+	get('how_jobs_one').style.color = "#000";
+	get('how_jobs_three').style.color = "#000";
+	get('how_jobs_one').style.background = "#fff";
+	get('how_jobs_three').style.background = "#fff";
+	get('how_jobs_panel_one').style.display = "none";
+	get('how_jobs_panel_two').style.display = "block";
+	get('how_jobs_panel_three').style.display = "none";
 }
 
-function jobs_switchFlatmate(){
-	get('how_jobs_flatmate').style.background = "#3a9bfa";
-	get('how_jobs_flatmate').style.color = "#fff";
-	get('how_jobs_jobs').style.color = "#000";
-	get('how_jobs_place').style.color = "#000";
-	get('how_jobs_jobs').style.background = "#fff";
-	get('how_jobs_place').style.background = "#fff";
-	get('how_jobs_panel_place').style.display = "none";
-	get('how_jobs_panel_jobs').style.display = "none";
-	get('how_jobs_panel_flatmate').style.display = "block";
+function jobs_switchThree(){
+	get('how_jobs_three').style.background = "#3a9bfa";
+	get('how_jobs_three').style.color = "#fff";
+	get('how_jobs_two').style.color = "#000";
+	get('how_jobs_one').style.color = "#000";
+	get('how_jobs_two').style.background = "#fff";
+	get('how_jobs_one').style.background = "#fff";
+	get('how_jobs_panel_one').style.display = "none";
+	get('how_jobs_panel_two').style.display = "none";
+	get('how_jobs_panel_three').style.display = "block";
 }
 
 
-//How Page functions start!
-function page_switchPlace(){
-	get('how_page_place').style.background = "#3a9bfa";
-	get('how_page_place').style.color = "#fff";
-	get('how_page_jobs').style.color = "#000";
-	get('how_page_flatmate').style.color = "#000";
-	get('how_page_jobs').style.background = "#fff";
-	get('how_page_flatmate').style.background = "#fff";
-	get('how_page_panel_place').style.display = "block";
-	get('how_page_panel_jobs').style.display = "none";
-	get('how_page_panel_flatmate').style.display = "none";
+//How Three functions start!
+function page_switchOne(){
+	get('how_page_one').style.background = "#3a9bfa";
+	get('how_page_one').style.color = "#fff";
+	get('how_page_two').style.color = "#000";
+	get('how_page_three').style.color = "#000";
+	get('how_page_two').style.background = "#fff";
+	get('how_page_three').style.background = "#fff";
+	get('how_page_panel_one').style.display = "block";
+	get('how_page_panel_two').style.display = "none";
+	get('how_page_panel_three').style.display = "none";
 }
 
-function page_switchJobs(){
-	get('how_page_jobs').style.background = "#3a9bfa";
-	get('how_page_jobs').style.color = "#fff";
-	get('how_page_place').style.color = "#000";
-	get('how_page_flatmate').style.color = "#000";
-	get('how_page_place').style.background = "#fff";
-	get('how_page_flatmate').style.background = "#fff";
-	get('how_page_panel_place').style.display = "none";
-	get('how_page_panel_jobs').style.display = "block";
-	get('how_page_panel_flatmate').style.display = "none";
+function page_switchTwo(){
+	get('how_page_two').style.background = "#3a9bfa";
+	get('how_page_two').style.color = "#fff";
+	get('how_page_one').style.color = "#000";
+	get('how_page_three').style.color = "#000";
+	get('how_page_one').style.background = "#fff";
+	get('how_page_three').style.background = "#fff";
+	get('how_page_panel_one').style.display = "none";
+	get('how_page_panel_two').style.display = "block";
+	get('how_page_panel_three').style.display = "none";
 }
 
-function page_switchFlatmate(){
-	get('how_page_flatmate').style.background = "#3a9bfa";
-	get('how_page_flatmate').style.color = "#fff";
-	get('how_page_jobs').style.color = "#000";
-	get('how_page_place').style.color = "#000";
-	get('how_page_jobs').style.background = "#fff";
-	get('how_page_place').style.background = "#fff";
-	get('how_page_panel_place').style.display = "none";
-	get('how_page_panel_jobs').style.display = "none";
-	get('how_page_panel_flatmate').style.display = "block";
+function page_switchThree(){
+	get('how_page_three').style.background = "#3a9bfa";
+	get('how_page_three').style.color = "#fff";
+	get('how_page_two').style.color = "#000";
+	get('how_page_one').style.color = "#000";
+	get('how_page_two').style.background = "#fff";
+	get('how_page_one').style.background = "#fff";
+	get('how_page_panel_one').style.display = "none";
+	get('how_page_panel_two').style.display = "none";
+	get('how_page_panel_three').style.display = "block";
 }
 
